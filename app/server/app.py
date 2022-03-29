22b0e4da11b084e69e28c7f289df6de87c7f60ed
mathiasg <mathiasg@stanford.edu>
2022-03-29 12:54:39 -0400

    ENH: Check mongo connection on startup, add CORS

--- app/server/app.py
+++ app/server/app.py
@@ -1,13 +1,35 @@
+import asyncio
+
 from fastapi import FastAPI, Depends
+from fastapi.middleware.cors import CORSMiddleware
 from strawberry.fastapi import GraphQLRouter
 
 from app import __version__
+from app.server.database import MongoClientHelper
 from app.server.schema.project import Project, project_schema
 
-app = FastAPI(version=__version__)
+def _create_app():
+    app = FastAPI(version=__version__)
+    graphql_app = GraphQLRouter(project_schema)
+    app.include_router(graphql_app, prefix="/graphql")
 
-graphql_app = GraphQLRouter(project_schema)
-app.include_router(graphql_app, prefix="/graphql")
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=["*"],
+        allow_methods=["*"],
+        allow_headers=["*"],
+    )
+    return app
+
+app = _create_app()
+
+@app.on_event("startup")
+async def startup():
+    app.db = MongoClientHelper()
+    try:
+        await asyncio.wait_for(app.db.is_valid(), 10)
+    except asyncio.TimeoutError:
+        print("Connection to MongoDB could not be made.")
 
 @app.get("/")
 async def root():
--- app/server/database.py
+++ app/server/database.py
@@ -1,2 +1,19 @@
+import asyncio
+import os
 from motor.motor_asyncio import AsyncIOMotorClient
 
+class MongoClientHelper:
+    """Helper class for writing to mongo database"""
+
+    def __init__(self):
+        self.client = AsyncIOMotorClient(
+            os.getenv("DB_HOSTNAME", "localhost"), os.getenv("DB_PORT", 27017),
+        )
+        # self.db = self.client[os.getenv("MASTER_ET_DB", "test_db")]
+        # self.requests = self.db["requests"]
+        # self.geoloc = self.db["geo"]
+
+    async def is_valid(self):
+        """Run mongo command to ensure valid connection"""
+        await asyncio.wait_for(self.client.admin.command("ping"), 10)
+        print("Database connection established!")
