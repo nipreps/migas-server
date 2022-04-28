f3f4e3bf10c17754428e0f63353682ad1874e590
mathiasg <mathiasg@stanford.edu>
2022-04-27 23:16:03 -0400

    ENH: Add proxy option

--- app/main.py
+++ app/main.py
@@ -9,6 +9,9 @@ def get_parser():
     parser.add_argument("--port", default=8000, help="server port")
     parser.add_argument("--workers", default=1, help="worker processes")
     parser.add_argument("--reload", action="store_true", help="Reload app on change (dev only)")
+    parser.add_argument(
+        "--proxy-headers", action="store_true", help="Accept incoming proxy headers"
+    )
     return parser
 
 
@@ -17,6 +20,7 @@ def main(argv=None):
 
     parser = get_parser()
     pargs = parser.parse_args(argv)
+    print(f"Starting server with the following options: {vars(pargs)}")
     uvicorn.run('app.server.app:app', **vars(pargs))
 
 
