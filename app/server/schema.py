3fb0a1269773b2a1158be41804fed5b3da8e42d7
mathiasg <mathiasg@stanford.edu>
2022-05-03 20:13:24 -0400

    FIX: Flesh out schema

--- app/server/schema.py
+++ app/server/schema.py
@@ -1,69 +1,35 @@
 import typing
+from uuid import UUID
 
 import strawberry
+from strawberry.scalars import JSON
 from strawberry.types import Info
 
-from app.server.types import (
-    Context,
-    DateTime,
-    Environment,
-    ProcessStatus,
-    Project,
-    User,
-)
-from app.server.utils import now, str_to_dt
-
-# Just for testing
-PROJECTS = [
-    Project('mgxd', 'test', '1.0', 'python', '3.10.4', str_to_dt("2022-03-28T13:04:24Z")),
-    Project(
-        'git',
-        'hub',
-        '2.0',
-        'python',
-        '3.8.3',
-        str_to_dt("2022-04-28T13:04:24Z"),
-        Context('darwin', user=0),
-    ),
-    Project(
-        'nipy',
-        'nipype',
-        '0.5',
-        'python',
-        '2.7.14',
-        str_to_dt("2022-04-21T13:04:24Z"),
-        Context('linux', 1, 2),
-    ),
-    Project(
-        'nipreps',
-        'nibabies',
-        '22.0.2',
-        'python',
-        '3.9.10',
-        str_to_dt("2022-04-28T13:00:24Z"),
-        Context(user=1),
-    ),
-]
+from app.server.fetchers import fetch_project_info
+from app.server.types import PROJECTS, Context, DateTime, Process, Project, ProjectInput
+from app.server.utils import now
 
 
 @strawberry.type
 class Query:
     @strawberry.field
-    def get_projects(self, info: Info) -> typing.List[Project]:
+    async def get_projects(self, info: Info) -> typing.List[Project]:
         request = info.context['request']
         print(f"Hello person at {request.client.host}!")
         return PROJECTS
 
     # This is the query we want!
     @strawberry.field
-    def get_project_from_name(self, name: str) -> Project:
+    async def get_project_from_name(self, name: str) -> Project:
         for p in PROJECTS:
             if p.name == name:
                 return p
 
     # and this!
     @strawberry.field
-    def from_date_range(self, date0: DateTime, date1: DateTime = None) -> typing.List[Project]:
+    async def from_date_range(
+        self, date0: DateTime, date1: DateTime = None
+    ) -> typing.List[Project]:
         # date0 = Date.str_to_dt(date0)
         if date1 is None:
             date1 = now()
@@ -81,19 +47,52 @@ class Query:
 @strawberry.type
 class Mutation:
     @strawberry.mutation
-    def add_project(
-        self,
-        owner: str,
-        repo: str,
-        version: str,
-        language: str,
-        language_version: str,
-    ) -> Project | None:
+    async def add_project(self, p: ProjectInput) -> JSON:
+        print(p)
+        print(f"Inserting in {p.owner}/{p.repo} database")
+        print(f"{p.status=}, {p.arguments=}")
+        print(type(p.arguments))
+        print(f"{p.session=}, {p.user_id=}")
 
-        p = Project(owner, repo, version, language, language_version, now())
-        print(f"Adding project {owner}/{repo} to DB")
-        PROJECTS.append(p)
-        return p
+        print(f"{p.owner=}, {p.repo=}")
+        project = Project(
+            owner=p.owner,
+            repo=p.repo,
+            version=p.version,
+            language=p.language,
+            language_version=p.language_version,
+            session=p.session,
+            timestamp=now(),
+            context=Context(
+                user_id=p.user_id,
+                user_type=p.user_type,
+                platform=p.platform,
+                container=p.container,
+            ),
+            process=Process(status=p.status),
+        )
+        PROJECTS.append(project)
+        fetched = await fetch_project_info(p.owner, p.repo)
+
+        # we should return the
+        # most up to date version
+        # as soon as possible >>
+        # For the rest of the behavior,
+        # add as FastAPI background tasks:
+        # - geoloc lookup
+        # - adding to database
+        return {
+            "success": True,
+            "latest_version": fetched["version"],
+            "bad_versions": [],
+            "message": [],
+        }
 
 
 SCHEMA = strawberry.Schema(query=Query, mutation=Mutation)
+
+
+def _validate_string(s, char_lim=16):
+    if len(s) > char_lim:
+        print(f"String {s} is over character limit")
+    return s[:char_lim]
