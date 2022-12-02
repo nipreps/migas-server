339fc2eeb93fa4d655f5049df4a05958e0802614
mathiasg <mathiasg@stanford.edu>
2022-12-02 14:10:34 -0500

    MAINT: Move to flit-scm versioning

--- setup.py
+++
@@ -1,9 +0,0 @@
-import versioneer
-from setuptools import setup
-
-if __name__ == "__main__":
-    setup(
-        name="migas_server",
-        version=versioneer.get_version(),
-        cmdclass=versioneer.get_cmdclass(),
-    )
