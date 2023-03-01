e9c2cdb412ac79b5e5d8f3fd9815f5ec24295538
mathiasg <mathiasg@stanford.edu>
2023-03-01 00:12:37 -0500

    MAINT: Relax request strictness

--- .maint/start-server-local.sh
+++ .maint/start-server-local.sh
@@ -1,5 +1,7 @@
 export MIGAS_REDIS_URI='redis://localhost'
 export DATABASE_URL='postgres://localhost/migas'
 export MIGAS_DEBUG=1
+export MIGAS_MAX_REQUESTS_PER_WINDOW=50
+export MIGAS_MAX_REQUEST_SIZE=3000
 
 migas-server --reload
