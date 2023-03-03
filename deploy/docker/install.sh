629ac8f1948dae375824c4f7c3841950c2581a40
mathiasg <mathiasg@stanford.edu>
2023-03-03 17:44:31 -0500

    CI: Fix src path, ensure pytest is available

--- deploy/docker/install.sh
+++ deploy/docker/install.sh
@@ -5,10 +5,10 @@ BUILDTYPE=$1
 case $BUILDTYPE in
 release)
     pip install --no-cache-dir pip-tools && pip-sync stable-requirements.txt;;
-test)
-    pip install --no-cache-dir /src/migas[test];;
 latest)
     pip install --no-cache-dir /src/migas;;
+latest-test)
+    pip install --no-cache-dir /src/migas[test];;
 *)
     echo "Unknown command" && exit 1;;
 esac
