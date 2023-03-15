47e35c0fb834c6c86410dfe101b67b23486c6424
mathiasg <mathiasg@stanford.edu>
2023-03-15 16:32:37 -0400

    DKR: Always install package to generate _version.py

--- deploy/docker/install.sh
+++ deploy/docker/install.sh
@@ -3,17 +3,27 @@
 BUILDTYPE=$1
 DEPLOYSERVER=$2
 
+# First update pip
+pip install --no-cache-dir pip
+
 case $BUILDTYPE in
 release)
-    pip install --no-cache-dir pip-tools && pip-sync stable-requirements.txt;;
+    pip install --no-cache-dir pip-tools
+    pip-sync stable-requirements.txt
+    pip install --no-cache-dir /src
+    ;;
 latest)
-    pip install --no-cache-dir /src;;
+    pip install --no-cache-dir /src
+    ;;
 latest-test)
-    pip install --no-cache-dir /src[test];;
+    pip install --no-cache-dir /src[test]
+    ;;
 *)
-    echo "Unknown command" && exit 1;;
+    echo "Unknown command"
+    exit 1
+    ;;
 esac
 
 if [ "$DEPLOYSERVER" = "gunicorn" ]; then
-    pip install gunicorn
+    pip install --no-cache-dir gunicorn
 fi
